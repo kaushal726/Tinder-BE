609ad54d955880448a8ff5d1c35f956654c1acad
kaushal <kausraj11@gmail.com>
2024-10-14 17:56:27 +0530

    add

--- src/app.js
+++ src/app.js
@@ -6,9 +6,18 @@ app.use("/test", (req, res) => {
   res.json({ message: "This is a test endpoint" });
 });
 
-app.use("/test", (req, res) => {
-  res.json({ message: "This is a test endpoint" });
-});
+// app.use("/test", (req, res) => {
+//   res.json({ message: "This is a test endpoint" });
+// });
+
+// app.get("/test/:name", (req, res) => {
+//   console.log(req.query);
+//   res.json({ message: "This is a test endpoint" });
+// });
+
+// app.post("/test", (req, res) => {
+//   res.json({ message: "This is a test endpoint" });
+// });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
